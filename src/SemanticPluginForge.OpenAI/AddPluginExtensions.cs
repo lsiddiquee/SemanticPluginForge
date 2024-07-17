6fb452cb603625baf785628c28394a48a2b4a49d
Likhan Siddiquee <likhan.siddiquee@gmail.com>
2024-07-17 21:41:38 +0200

    Add open api support

---
+++ src/SemanticPluginForge.OpenAI/AddPluginExtensions.cs
@@ -0,0 +1,101 @@
+﻿#pragma warning disable SKEXP0040
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.OpenApi;
+
+namespace SemanticPluginForge;
+
+public static class AddPluginExtensions
+{
+    public async static Task<KernelPlugin> AddFromOpenApiWithMetadataAsync(this IServiceProvider serviceProvider, string pluginName, string filePath, OpenApiFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var kernel = new Kernel(serviceProvider);
+
+        return await kernel.AddFromOpenApiWithMetadataAsync(pluginName, filePath, executionParameters, cancellationToken);
+    }
+
+    public async static Task<KernelPlugin> AddFromOpenApiWithMetadataAsync(this Kernel kernel, string pluginName, string filePath, OpenApiFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var kernelPlugin = await kernel.CreatePluginFromOpenApiAsync(pluginName, filePath, executionParameters, cancellationToken);
+
+        return kernel.AddPatchedKernelPluginWithMetadata(kernelPlugin);
+    }
+
+    public async static Task<KernelPlugin> AddFromOpenApiWithMetadataAsync(this IServiceProvider serviceProvider, string pluginName, Uri uri, OpenApiFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var kernel = new Kernel(serviceProvider);
+
+        return await kernel.AddFromOpenApiWithMetadataAsync(pluginName, uri, executionParameters, cancellationToken);
+    }
+
+    public async static Task<KernelPlugin> AddFromOpenApiWithMetadataAsync(this Kernel kernel, string pluginName, Uri uri, OpenApiFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var kernelPlugin = await kernel.CreatePluginFromOpenApiAsync(pluginName, uri, executionParameters, cancellationToken);
+
+        return kernel.AddPatchedKernelPluginWithMetadata(kernelPlugin);
+    }
+
+    public async static Task<KernelPlugin> AddFromOpenApiWithMetadataAsync(this IServiceProvider serviceProvider, string pluginName, Stream stream, OpenApiFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var kernel = new Kernel(serviceProvider);
+
+        return await kernel.AddFromOpenApiWithMetadataAsync(pluginName, stream, executionParameters, cancellationToken);
+    }
+
+    public async static Task<KernelPlugin> AddFromOpenApiWithMetadataAsync(this Kernel kernel, string pluginName, Stream stream, OpenApiFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var kernelPlugin = await kernel.CreatePluginFromOpenApiAsync(pluginName, stream, executionParameters, cancellationToken);
+
+        return kernel.AddPatchedKernelPluginWithMetadata(kernelPlugin);
+    }
+
+    public async static Task<KernelPlugin> AddFromOpenAIWithMetadataAsync(this IServiceProvider serviceProvider, string pluginName, string filePath, OpenAIFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var kernel = new Kernel(serviceProvider);
+
+        return await kernel.AddFromOpenAIWithMetadataAsync(pluginName, filePath, executionParameters, cancellationToken);
+    }
+
+    public async static Task<KernelPlugin> AddFromOpenAIWithMetadataAsync(this Kernel kernel, string pluginName, string filePath, OpenAIFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var kernelPlugin = await kernel.CreatePluginFromOpenAIAsync(pluginName, filePath, executionParameters, cancellationToken);
+
+        return kernel.AddPatchedKernelPluginWithMetadata(kernelPlugin);
+    }
+
+    public async static Task<KernelPlugin> AddFromOpenAIWithMetadataAsync(this IServiceProvider serviceProvider, string pluginName, Uri uri, OpenAIFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var kernel = new Kernel(serviceProvider);
+
+        return await kernel.AddFromOpenAIWithMetadataAsync(pluginName, uri, executionParameters, cancellationToken);
+    }
+
+    public async static Task<KernelPlugin> AddFromOpenAIWithMetadataAsync(this Kernel kernel, string pluginName, Uri uri, OpenAIFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var kernelPlugin = await kernel.CreatePluginFromOpenAIAsync(pluginName, uri, executionParameters, cancellationToken);
+
+        return kernel.AddPatchedKernelPluginWithMetadata(kernelPlugin);
+    }
+
+    public async static Task<KernelPlugin> AddFromOpenAIWithMetadataAsync(this IServiceProvider serviceProvider, string pluginName, Stream stream, OpenAIFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var kernel = new Kernel(serviceProvider);
+
+        return await kernel.AddFromOpenAIWithMetadataAsync(pluginName, stream, executionParameters, cancellationToken);
+    }
+
+    public async static Task<KernelPlugin> AddFromOpenAIWithMetadataAsync(this Kernel kernel, string pluginName, Stream stream, OpenAIFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var kernelPlugin = await kernel.CreatePluginFromOpenAIAsync(pluginName, stream, executionParameters, cancellationToken);
+
+        return kernel.AddPatchedKernelPluginWithMetadata(kernelPlugin);
+    }
+
+    public static KernelPlugin AddPatchedKernelPluginWithMetadata(this Kernel kernel, KernelPlugin kernelPlugin)
+    {
+        var patchedPlugin = kernel.Services.PatchKernelPluginWithMetadata(kernelPlugin);
+
+        kernel.Plugins.Add(patchedPlugin);
+
+        return patchedPlugin;
+    }
+}
