b5e4b2eb331f9bb87bfc1295fdc5938428ce03a4
Likhan Siddiquee <likhan.siddiquee@gmail.com>
2025-05-02 11:57:47 +0200

    adding run settings and updating to the latest sk (#9)

--- src/SemanticPluginForge.OpenApi/AddPluginExtensions.cs
+++ src/SemanticPluginForge.OpenApi/AddPluginExtensions.cs
@@ -5,7 +5,7 @@ using Microsoft.SemanticKernel.Plugins.OpenApi;
 namespace SemanticPluginForge;
 
 /// <summary>
-/// Extension methods for adding plugins from OpenAPI and OpenAI sources with metadata to the kernel's plugin collection.
+/// Extension methods for adding plugins from OpenAPI sources with metadata to the kernel's plugin collection.
 /// </summary>
 public static class AddPluginExtensions
 {
@@ -51,51 +51,6 @@ public static class AddPluginExtensions
     public async static Task<KernelPlugin> AddFromOpenApiWithMetadataAsync(this Kernel kernel, string pluginName, Stream stream, OpenApiFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
     {
         var kernelPlugin = await kernel.CreatePluginFromOpenApiAsync(pluginName, stream, executionParameters, cancellationToken);
-        return kernel.AddPatchedKernelPluginWithMetadata(kernelPlugin);
-    }
-
-    /// <summary>
-    /// Creates a plugin from an OpenAI metadata file, updates its metadata, and adds it to the kernel's plugin collection.
-    /// </summary>
-    /// <param name="kernel">The <see cref="Kernel"/> containing services, plugins, and other state for use throughout the operation.</param>
-    /// <param name="pluginName">The name of the plugin.</param>
-    /// <param name="filePath">The file path to the OpenAI metadata file.</param>
-    /// <param name="executionParameters">Optional execution parameters for the plugin.</param>
-    /// <param name="cancellationToken">A cancellation token to observe while waiting for the task to complete.</param>
-    /// <returns>A task representing the asynchronous operation. The task result contains the added <see cref="KernelPlugin"/>.</returns>
-    public async static Task<KernelPlugin> AddFromOpenAIWithMetadataAsync(this Kernel kernel, string pluginName, string filePath, OpenAIFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
-    {
-        var kernelPlugin = await kernel.CreatePluginFromOpenAIAsync(pluginName, filePath, executionParameters, cancellationToken);
-        return kernel.AddPatchedKernelPluginWithMetadata(kernelPlugin);
-    }
-
-    /// <summary>
-    /// Creates a plugin from an OpenAI metadata URI, updates its metadata, and adds it to the kernel's plugin collection.
-    /// </summary>
-    /// <param name="kernel">The <see cref="Kernel"/> containing services, plugins, and other state for use throughout the operation.</param>
-    /// <param name="pluginName">The name of the plugin.</param>
-    /// <param name="uri">The URI to the OpenAI metadata.</param>
-    /// <param name="executionParameters">Optional execution parameters for the plugin.</param>
-    /// <param name="cancellationToken">A cancellation token to observe while waiting for the task to complete.</param>
-    /// <returns>A task representing the asynchronous operation. The task result contains the added <see cref="KernelPlugin"/>.</returns>
-    public async static Task<KernelPlugin> AddFromOpenAIWithMetadataAsync(this Kernel kernel, string pluginName, Uri uri, OpenAIFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
-    {
-        var kernelPlugin = await kernel.CreatePluginFromOpenAIAsync(pluginName, uri, executionParameters, cancellationToken);
-        return kernel.AddPatchedKernelPluginWithMetadata(kernelPlugin);
-    }
-
-    /// <summary>
-    /// Creates a plugin from an OpenAI metadata stream, updates its metadata, and adds it to the kernel's plugin collection.
-    /// </summary>
-    /// <param name="kernel">The <see cref="Kernel"/> containing services, plugins, and other state for use throughout the operation.</param>
-    /// <param name="pluginName">The name of the plugin.</param>
-    /// <param name="stream">The stream representing the OpenAI metadata.</param>
-    /// <param name="executionParameters">Optional execution parameters for the plugin.</param>
-    /// <param name="cancellationToken">A cancellation token to observe while waiting for the task to complete.</param>
-    /// <returns>A task representing the asynchronous operation. The task result contains the added <see cref="KernelPlugin"/>.</returns>
-    public async static Task<KernelPlugin> AddFromOpenAIWithMetadataAsync(this Kernel kernel, string pluginName, Stream stream, OpenAIFunctionExecutionParameters? executionParameters = null, CancellationToken cancellationToken = default(CancellationToken))
-    {
-        var kernelPlugin = await kernel.CreatePluginFromOpenAIAsync(pluginName, stream, executionParameters, cancellationToken);
         return kernel.AddPatchedKernelPluginWithMetadata(kernelPlugin);
     }
 
